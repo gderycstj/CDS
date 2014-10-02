acf5fd5974d90e33b67582c85fdb9c2347ed3558
zladix <vincent.leclerc21@hotmail.fr>
2014-10-02 14:40:54 -0400

    Je COMMIT NOW

---
+++ CDS/CDS/Écran/menuInstruction.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CDS
+{
+    /// <summary>
+    /// Logique d'interaction pour menuInstruction.xaml
+    /// </summary>
+    public partial class menuInstruction : Window
+    {
+        public menuInstruction()
+        {
+            InitializeComponent();
+            
+     
+        }
+
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            menuPrincipal menuP = new menuPrincipal();
+            menuP.Show();
+            Close();
+        }
+
+        private void btnBut_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "But";
+            txtContenu.TextAlignment = TextAlignment.Center;
+            txtContenu.Text = "La chambre de survie vous confrontera à plusieurs niveaux de survie qui augmente en difficulté. En outre, si la peur vous hante, vous pourriez toujours vous entrainer face aux plus puissants poursuivants avant de relevé le défi.\n Dans la chambre de survie, vous allez devoir survivre à des vagues de poursuivants de plus en plus acharnée au fil du temps et des niveaux. De plus, plusieurs variétés de poursuivants seront disponibles et s'ajouterons aux poursuivants de « base » présente dans les premiers niveaux. \n"+
+            "Sauvegardez votre  partie, partagez vos score mémorables, surpassez l'élite des survivants. Le défi sera de taille. Et si vous finissez par le vaincre, d'autres chambres seront assemblées par vous et les autres survivants. Saurez-vous survivre à \n"+
+            "la chambre de survie?";
+        }
+
+        private void btnCommandes_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "Commandes";
+            txtContenu.Text = "blablablabla";
+        }
+
+        private void btnPoursuivants_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "Poursuivants";
+            txtContenu.Text = "blablablabla";
+        }
+
+        private void btnObjets_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "Objets";
+            txtContenu.Text = "blablablabla";
+        }
+
+    }
+}
---
+++ CDS/CDS/Écran/menuJouer.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CDS
+{
+    /// <summary>
+    /// Logique d'interaction pour menuJouer.xaml
+    /// </summary>
+    public partial class menuJouer : Window
+    {
+        public menuJouer()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnConnexion_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+
+        private void btnRetourCDS_Click(object sender, RoutedEventArgs e)
+        {
+            menuPrincipal menuP = new menuPrincipal();
+            menuP.Show();
+            Close();
+        }
+
+        private void btnDemarrer_Click(object sender, RoutedEventArgs e)
+        {
+            return;
+        }
+    }
+}
