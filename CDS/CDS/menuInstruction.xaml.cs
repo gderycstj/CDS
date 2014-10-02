99bfe6eaa2ed1d6fbc1ec477460c836064e63052
zladix <vincent.leclerc21@hotmail.fr>
2014-10-02 14:05:38 -0400

    Écran Du jeux
    Ajouter

---
+++ CDS/CDS/menuInstruction.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CDS
+{
+    /// <summary>
+    /// Logique d'interaction pour menuInstruction.xaml
+    /// </summary>
+    public partial class menuInstruction : Window
+    {
+        public menuInstruction()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRetour_Click(object sender, RoutedEventArgs e)
+        {
+            menuPrincipal menuP = new menuPrincipal();
+            menuP.Show();
+            Close();
+        }
+
+        private void btnBut_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "But";
+        }
+
+        private void btnCommandes_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "Commandes";
+        }
+
+        private void btnPoursuivants_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "Poursuivants";
+        }
+
+        private void btnObjets_Click(object sender, RoutedEventArgs e)
+        {
+            txtTitreInstruc.Text = "Objets";
+        }
+
+    }
+}
