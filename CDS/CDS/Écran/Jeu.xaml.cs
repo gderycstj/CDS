a85e4f94d6b1c094d2bf3f227fe53cf6cfa261b2
gderycstj <gabriodery@hotmail.com>
2014-10-16 21:35:13 -0400

    modif finale

--- CDS/CDS/Écran/Jeu.xaml.cs
+++ CDS/CDS/Écran/Jeu.xaml.cs
@@ -22,6 +22,69 @@ namespace CDS
         public Jeu()
         {
             InitializeComponent();
+            afficherGrilleJeu();
+        }
+        /// <summary>
+        /// cette fonction devrait théorquement afficher tout les éléments du jeu mais je n'ai fait que le déplacement du joueur
+        /// </summary>
+        void afficherGrilleJeu()
+        {
+            DéplacerJoueur();
+        }
+
+        /// <summary>
+        /// Fonction pour déplacer un joueur
+        /// </summary>
+        void DéplacerJoueur() 
+        {
+            //va éffacer la grille a chaque déplacement et va réafficher le joueur à sa nouvelle position
+            grillePrincipale.Children.Clear();
+            Image img = new Image();
+            img = Globale.j1.obtenirImage();
+            Grid.SetColumn(img, Globale.j1.positionJoueur.pos.posX);
+            Grid.SetRow(img, Globale.j1.positionJoueur.pos.posY);
+            grillePrincipale.Children.Add(img);
+        
+        }
+        
+        //fonctionde déplacement pour les boutons, haut veux dire déplacement haut... fait aussi les validations pour pas dépasser la grille
+        private void haut(object sender, RoutedEventArgs e)
+        {
+            if (Globale.j1.positionJoueur.pos.posY > 1)
+            {
+                Globale.j1.positionJoueur.pos.posY = Globale.j1.positionJoueur.pos.posY - 1;
+                DéplacerJoueur();
+            }
+
+        }
+
+        private void bas(object sender, RoutedEventArgs e)
+        {
+            if (Globale.j1.positionJoueur.pos.posY < GrilleDeJeu.TailleGrille - 2)
+            {
+                Globale.j1.positionJoueur.pos.posY = Globale.j1.positionJoueur.pos.posY + 1;
+                DéplacerJoueur();
+            }
+        }
+
+        private void droite(object sender, RoutedEventArgs e)
+        {
+            if (Globale.j1.positionJoueur.pos.posX < GrilleDeJeu.TailleGrille - 2)
+            {
+                Globale.j1.positionJoueur.pos.posX = Globale.j1.positionJoueur.pos.posX + 1;
+                DéplacerJoueur();
+            }
+        }
+
+        private void gauche(object sender, RoutedEventArgs e)
+        {
+
+            if (Globale.j1.positionJoueur.pos.posX > 1)
+            {
+                Globale.j1.positionJoueur.pos.posX = Globale.j1.positionJoueur.pos.posX - 1;
+                DéplacerJoueur();
+            }
+
         }
     }
 }
