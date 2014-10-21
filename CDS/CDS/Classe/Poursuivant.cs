aeeef7cca6b8335e41b2ed2d9b3cfb9ea204a5f7
william duperré <duperrewilliam@hotmail.com>
2014-10-21 12:51:20 -0400

    CMD partie 2
    J'ai continuer a developper la lecture des listeCMD j'ai placer les
    mouvement et les ifjoueur selon la direction de du poursuivant

--- CDS/CDS/Classe/Poursuivant.cs
+++ CDS/CDS/Classe/Poursuivant.cs
@@ -45,24 +45,42 @@ namespace CDS
             }
 
             //Lecture de CMD   
-
+            lire(CMD);
 
 
             return true;
         }
 
 
+
+        private void lire(char[] CMD)
+        {
+            string newCMD=CMD.ToString();
+
+            lire(newCMD);
+        }
+
         /// <summary>
         /// Lit la chaine de commande
         /// </summary>
         /// <param name="CMD">la chaine de commande</param>
-        private void lire(string CMD, List<Effet> listeEffet )
+        private void lire(string CMD)
         {
             char lettre;
             char lettre2;
             char lettre3;
+
+            char[] sousCMD={'\0'}; //Une sous liste pour savoir quoi faire pour le if
+
             int numero;
             int iLecture = 0;
+            int incrementation=0;// la profondeur et quantiter de if dans un if dans un if dans... etc.
+            int iEcriture=0;//
+
+            bool endif=false;
+
+
+
             while (CMD != "")
             {
 
@@ -80,18 +98,90 @@ namespace CDS
                         {
                             //Haut
                             case 'h':
+                                switch(direction)
+                                { 
+                                    case 1:
+                                        mouvementSud();
+                                        break;
+
+                                    case 2:
+                                        mouvementOuest();
+                                        break;
+
+                                    case 3:
+                                        mouvementNord();
+                                        break;
+
+                                    case 4:
+                                        mouvementEst();
+                                        break;
+                                }
                                 break;
 
                             //Droite
                             case 'd':
+                                switch (direction)
+                                {
+                                    case 2:
+                                        mouvementSud();
+                                        break;
+
+                                    case 3:
+                                        mouvementOuest();
+                                        break;
+
+                                    case 4:
+                                        mouvementNord();
+                                        break;
+
+                                    case 1:
+                                        mouvementEst();
+                                        break;
+                                }
                                 break;
 
                             //Bas
                             case 'b':
+                                switch (direction)
+                                {
+                                    case 3:
+                                        mouvementSud();
+                                        break;
+
+                                    case 4:
+                                        mouvementOuest();
+                                        break;
+
+                                    case 1:
+                                        mouvementNord();
+                                        break;
+
+                                    case 2:
+                                        mouvementEst();
+                                        break;
+                                }
                                 break;
 
                             //Gauche
                             case 'g':
+                                switch (direction)
+                                {
+                                    case 4:
+                                        mouvementSud();
+                                        break;
+
+                                    case 1:
+                                        mouvementOuest();
+                                        break;
+
+                                    case 2:
+                                        mouvementNord();
+                                        break;
+
+                                    case 3:
+                                        mouvementEst();
+                                        break;
+                                }
                                 break;
                         }
                         break;
@@ -100,8 +190,17 @@ namespace CDS
                     case 'E':
                         numero = CMD[iLecture];
                         iLecture++;
+                         numero -= '0'; //du code ascii au numero lui-même
 
-                        numero -= 48; //du code ascii au numero lui-même
+                        //si c'est un effet entre 10 et 99
+                        if(CMD[iLecture]>='0' && CMD[iLecture]<='9')
+                        {
+                            numero = numero*10;//le nombre précédent est a la position des dizaine
+                            numero = numero + (CMD[iLecture]-'0');
+                            iLecture++;
+                        }
+
+                       
 
                         //on lance l'effet commandé
                         listeEffet[numero].action();
@@ -110,9 +209,50 @@ namespace CDS
 
                     //=====If=====
                     case 'I':
+                        //on prend en note la lettre qui suit pour...
                         lettre2 = CMD[iLecture];
                         iLecture++;
 
+                        //...pouvoir isolet la/les actions en cas de true
+                        endif=false;
+                        iEcriture=0;
+                        while(endif)
+                        {
+                            switch(CMD[iLecture])
+                            {
+                                case '{':
+                                    //Acolade qui ne viennent pas du if en cours de traitage
+                                    //on veut l'acolade dans la sousliste
+                                    if(incrementation!=0)
+                                    {
+                                        sousCMD[iEcriture] = CMD[iLecture];
+                                        iEcriture++;
+                                    }
+                                    incrementation++;
+                                    break;
+
+                                case '}':
+                                    //idem de l'autre acolade
+                                    if(incrementation!=1)
+                                    {
+                                        sousCMD[iEcriture] = CMD[iLecture];
+                                        iEcriture++;
+                                    }
+                                    incrementation--;
+                                    break;
+
+                                default:
+                                    sousCMD[iEcriture]=CMD[iLecture];
+                                    iEcriture++;
+                                    break;
+                            }
+
+                            iLecture++;
+                            if(incrementation<=0)
+                                endif=true;
+                        }
+
+                        //on lit la lettre prise en réserve plus tôt pour savoir quoi vérifier
                         switch(lettre2)
                         {
                             //=====Joueur=====
@@ -124,18 +264,108 @@ namespace CDS
                                 {
                                     //haut
                                     case 'h':
+                                        switch (direction)
+                                        {
+                                            case 1:
+                                                if (ifJoueurSud())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 2:
+                                                if (ifJoueurOuest())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 3:
+                                                if (ifJoueurNord())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 4:
+                                                if (ifJoueurEst())
+                                                    lire(sousCMD);
+                                                break;
+                                        }
                                         break;
                                     //Droit
                                     case 'd':
+                                        switch (direction)
+                                        {
+                                            case 2:
+                                                if (ifJoueurSud())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 3:
+                                                if (ifJoueurOuest())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 4:
+                                                if (ifJoueurNord())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 1:
+                                                if (ifJoueurEst())
+                                                    lire(sousCMD);
+                                                break;
+                                        }
                                         break;
                                     //Bas
                                     case 'b':
+                                        switch (direction)
+                                        {
+                                            case 3:
+                                                if (ifJoueurSud())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 4:
+                                                if (ifJoueurOuest())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 1:
+                                                if (ifJoueurNord())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 2:
+                                                if (ifJoueurEst())
+                                                    lire(sousCMD);
+                                                break;
+                                        }
                                         break;
                                     //Gauche
                                     case 'g':
+                                        switch (direction)
+                                        {
+                                            case 4:
+                                                if(ifJoueurSud())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 1:
+                                                if(ifJoueurOuest())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 2:
+                                                if(ifJoueurNord())
+                                                    lire(sousCMD);
+                                                break;
+
+                                            case 3:
+                                                if(ifJoueurEst())
+                                                    lire(sousCMD);
+                                                break;
+                                        }
                                         break;
                                     //Proximité
                                     case 'p':
+                                        if(ifJoueurProche())
+                                            lire(sousCMD);
                                         break;
                                 }
                                 break;
@@ -198,6 +428,78 @@ namespace CDS
             }
         }
 
+        /// <summary>
+        /// WIP mouvement vers le haut
+        /// </summary>
+        void mouvementNord()
+        {
+
+        }
+
+        /// <summary>
+        /// WIP mouvement vers le bas
+        /// </summary>
+        void mouvementSud()
+        {
+
+        }
+
+        /// <summary>
+        /// WIP mouvement vers la droite
+        /// </summary>
+        void mouvementEst()
+        {
+
+        }
+
+        /// <summary>
+        /// WIP mouvement vers la gauche
+        /// </summary>
+        void mouvementOuest()
+        {
+
+        }
+
+        /// <summary>
+        /// Teste si le joueur est au nord de l'entite
+        /// </summary>
+        /// <returns>Si le joueur est au nord de l'entite</returns>
+        bool ifJoueurNord()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Teste si le joueur est au sud de l'entite
+        /// </summary>
+        /// <returns>Si le joueur est au sud de l'entite</returns>
+        bool ifJoueurSud()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Teste si le joueur est à l'est de l'entite
+        /// </summary>
+        /// <returns>Si le joueur est à l'est de l'entite</returns>
+        bool ifJoueurEst()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Teste si le joueur est à l'ouest de l'entite
+        /// </summary>
+        /// <returns>Si le joueur est à l'ouest de l'entite</returns>
+        bool ifJoueurOuest()
+        {
+            return true;
+        }
+
+        bool ifJoueurProche()
+        {
+            return true;
+        }
 
         //détruire l'entité et faire les modification associé
         public override bool mort()
